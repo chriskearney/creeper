f492c4b38c992cfa69cd38b0937c51a890ba8fe4
Chris K <chrisk@Chriss-MacBook-Pro.local>
2018-03-13 16:01:51 -0700

    more logging i guess

--- src/main/java/events/CreeperToSSEEventListener.java
+++ src/main/java/events/CreeperToSSEEventListener.java
@@ -32,6 +32,7 @@ public class CreeperToSSEEventListener implements CreeperEventListener {
                 eventBuilder.id(timestamp);
                 eventBuilder.name("creeperEvent");
                 eventBuilder.data(objectMapper.writeValueAsString(creeperEvent));
+                log.info("ABOUT TO EMIT AN EVENT!!!");
                 eventOutput.write(eventBuilder.build());
             }
         } catch (Exception e) {
