ccad17cbfcf6a18222a728964da90a4c51746182
Chris K <chrisk@Chriss-MacBook-Pro.local>
2018-03-13 11:22:56 -0700

    fixing bad json and logging the exception.

--- src/main/java/com/comandante/creeper/player/PlayerManager.java
+++ src/main/java/com/comandante/creeper/player/PlayerManager.java
@@ -98,7 +98,7 @@ public class PlayerManager {
                     .build();
             listenerService.post(build);
         } catch (Exception e) {
-            log.error("Problem emitting playermetadata event.");
+            log.error("Problem emitting playermetadata event.", e);
         }
     }
 
