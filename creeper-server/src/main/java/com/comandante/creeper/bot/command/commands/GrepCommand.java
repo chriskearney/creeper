430d97b80279e31832cae8fd744847d284c2eaf3
Chris Kearney <ckearney@pm.me>
2020-11-17 02:58:25 -0800

    YES flag

--- creeper-server/src/main/java/com/comandante/creeper/bot/command/commands/GrepCommand.java
+++ creeper-server/src/main/java/com/comandante/creeper/bot/command/commands/GrepCommand.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Sets;
 import org.pircbotx.Colors;
 import org.testng.collections.Lists;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -23,8 +24,20 @@ public class GrepCommand extends BotCommand {
     @Override
     public List<String> process() {
         String keywordQuery = args.remove(0);
+        Optional<String> yesFlag = Optional.empty();
+        if (!args.isEmpty()) {
+            yesFlag = Optional.ofNullable(args.remove(0));
+        }
         List<QuoteManager.IrcQuote> byKeyword = botCommandManager.getQuoteManager().grep(keywordQuery);
-        botCommandManager.getQuoteProcessor().addIrcQuotes(byKeyword, Optional.ofNullable(getMessageEvent()));
+        if (byKeyword.size() > 100) {
+            if (yesFlag.equals("-YES")) {
+                botCommandManager.getQuoteProcessor().addIrcQuotes(byKeyword, Optional.ofNullable(getMessageEvent()));
+            } else {
+                int totalSeconds = byKeyword.size() * 2;
+                double minutes = ((double) totalSeconds) / 60;
+                return Collections.singletonList("Query will take approximately " + minutes + "minutes. Add -YES to to your command in order to proceed.");
+            }
+        }
         return Lists.newArrayList();
     }
 }
