cef2d6e0dc4ba6093d8fc83c28ea14b0c943fd95
Chris Kearney <chris@kearneymail.com>
2021-01-28 00:58:56 -0800

    very quick yahoo integration

--- creeper-server/src/main/java/com/comandante/creeper/bot/AlphaVantageManager.java
+++ creeper-server/src/main/java/com/comandante/creeper/bot/AlphaVantageManager.java
@@ -4,7 +4,10 @@ import org.devotionit.vantage.AlphaVantageClient;
 import org.devotionit.vantage.core.request.QuoteRequest;
 import org.devotionit.vantage.core.request.param.Symbol;
 import org.devotionit.vantage.core.response.QuoteResponse;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
 
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.concurrent.CompletableFuture;
@@ -21,20 +24,19 @@ public class AlphaVantageManager {
     }
 
     public String getStockPrice(String symbol) {
-        CompletableFuture<QuoteResponse> async = alphaVantageClient.getAsync(new QuoteRequest(Symbol.from(symbol)));
         try {
-            QuoteResponse quoteResponse = async.get(10L, TimeUnit.SECONDS);
-            String quoteDetails = quoteResponse.getSymbol() + "[" +
-                    "price " + currencyFormatter.format(quoteResponse.getPrice()) +
-                    " | open " + currencyFormatter.format(quoteResponse.getOpen()) +
-                    " | high " + currencyFormatter.format(quoteResponse.getHigh()) +
-                    " | low " + currencyFormatter.format(quoteResponse.getLow()) + "" +
-                    " | volume " + numberCommaFormatter.format(quoteResponse.getVolume()) +
-                    " | change " + quoteResponse.getChange() +
-                    " | percent " + quoteResponse.getChangePercent() +
+            Stock stock = YahooFinance.get(symbol, true);
+            String quoteDetails = stock.getQuote().getSymbol() + "[" +
+                    "price " + currencyFormatter.format(stock.getQuote().getPrice()) +
+                    " | open " + currencyFormatter.format(stock.getQuote().getOpen()) +
+                    " | high " + currencyFormatter.format(stock.getQuote().getDayHigh()) +
+                    " | low " + currencyFormatter.format(stock.getQuote().getDayLow()) + "" +
+                    " | volume " + numberCommaFormatter.format(stock.getQuote().getVolume()) +
+                    " | change " + stock.getQuote().getChange() +
+                    " | percent " + stock.getQuote().getChangeInPercent() +
                     "]";
             return quoteDetails;
-        } catch (Exception e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
